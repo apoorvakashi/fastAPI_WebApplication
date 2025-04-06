60fb666456b52f00f0788bba0454b5289fef25ae
Apoorva Kashi <apoorvakashi@Apoorvas-MacBook-Air-2.local>
2025-04-06 01:25:01 -0400

    database service fix

--- tests/database.py
+++ tests/database.py
@@ -9,7 +9,7 @@ from fastapi.testclient import TestClient
 from app.main import app
 
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
