30f8318b6412661d162982892a1698656f44d671
Apoorva Kashi <apoorvakashi@Apoorvas-MacBook-Air-2.local>
2025-04-05 23:10:22 -0400

    Tests added for Posts and Votes

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -6,11 +6,12 @@ router = APIRouter(prefix="/votes", tags = ["Votes"])
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def vote(vote: schemas.Vote, db:Session=Depends(database.get_db), current_user = Depends(oauth2.get_current_user)):
-    vote_query = db.query(models.Vote).filter(models.Vote.user_id == current_user.id, models.Vote.post_id == vote.post_id)
-    found_vote = vote_query.first()
+    
     post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post does not exist")
+    vote_query = db.query(models.Vote).filter(models.Vote.user_id == current_user.id, models.Vote.post_id == vote.post_id)
+    found_vote = vote_query.first()
 
     if vote.dir == 1:
         if found_vote:
